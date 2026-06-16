fcfc53d35a1d7239a1b9de390eab0cac4cb9f608
amblanco99 <am.blanco@uniandes.edu.co>
2026-06-16 12:38:28 -0500

    fix: include all html pages in vite build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/ColonialExperimento/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tablas: resolve(__dirname, 'tablas.html'),
+        about: resolve(__dirname, 'about.html'),
+        casos: resolve(__dirname, 'casos.html'),
+      }
+    }
+  }
 })
